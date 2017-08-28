bf9d09dfc608cbaa64ae0a41abfac265bc255905
ruixman <ruixman@qq.com>
2017-08-28 17:23:04 +0800

    clear code

--- src/main/java/com/hfzs/framework/core/web/ContextConfig.java
+++ src/main/java/com/hfzs/framework/core/web/ContextConfig.java
@@ -25,18 +25,4 @@ import java.io.IOException;
 @EnableJpaRepositories(basePackages = {"com.hfzs.biz.*.repository"}, repositoryFactoryBeanClass = MyJpaRepositoryFactoryBean.class)
 @ComponentScan({"com.hfzs.biz.*.service.impl", "com.hfzs.biz.*.controller","com.hfzs.biz.*.service"})
 class ContextConfig {
-//class ContextConfig extends AbstractProcessEngineAutoConfiguration {
-//static final String NAME = "master";
-//    @Bean
-//    public SpringProcessEngineConfiguration springProcessEngineConfiguration(
-//            @Qualifier(NAME + "DataSource") DataSource dataSource,
-//            @Qualifier(NAME + "TransactionManager") PlatformTransactionManager transactionManager,
-//            SpringAsyncExecutor springAsyncExecutor) throws IOException {
-//        return this.baseSpringProcessEngineConfiguration(dataSource, transactionManager, springAsyncExecutor);
-//    }
-
-//    作者：会灰的大飞狼
-//    链接：http://www.jianshu.com/p/6c9882693f35
-//    來源：简书
-//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。非商业转载请注明出处
 }
