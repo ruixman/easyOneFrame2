2e517daee789a0c107fbfadbfd42ac69ed455a21
ruixman <ruixman@qq.com>
2017-09-01 13:41:06 +0800

    clear code

--- src/main/java/com/hfzs/framework/domain/dto/PageDto.java
+++ src/main/java/com/hfzs/framework/domain/dto/PageDto.java
@@ -38,14 +38,6 @@ public class PageDto<T> {
 		return sort;
 	}
 
-//	public Object getSearchObj() {
-//		return searchObj;
-//	}
-//
-//	public void setSearchObj(Object searchObj) {
-//		this.searchObj = searchObj;
-//	}
-
 	public void setSort(String sort) {
 		this.sort = sort;
 	}
@@ -81,15 +73,7 @@ public class PageDto<T> {
 	public void setPage(Integer page) {
 		this.page = page;
 	}
-	
-//	public List<T> getData() {
-//		return data;
-//	}
-//
-//	public void setData(List<T> data) {
-//		this.data = data;
-//	}
-	
+
 	@Override
 	public String toString() {
 		return "Page [limit=" + limit + ", start=" + start + ", page=" + page + ", data="+ "]";
@@ -102,85 +86,5 @@ public class PageDto<T> {
 	public void setTotal(Integer total) {
 		this.total = total;
 	}
-
-//	public Sort getSort() {
-//		return sort;
-//	}
-//
-//	public void setSort(Sort sort) {
-//		this.sort = sort;
-//	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	//======datatable 控件
-//	private Integer sEcho=0;//当前页(默认为0)
-//	private Integer iTotalDisplayRecords;//总显示记录(总条数)
-//	private Integer iDisplayLength;//每页条数
-//	
-//	private Integer iTotalRecords;//全记录
-//	private List<T> data;
-//	
-////	private Map<String, Object> map;
-//	
-//	public Integer getiDisplayLength() {
-//		return iDisplayLength;
-//	}
-//
-//	public void setIDisplayLength(Integer iDisplayLength) {
-//		this.iDisplayLength = iDisplayLength;
-//	}
-//
-//	public Integer getiTotalDisplayRecords() {
-//		return iTotalDisplayRecords;
-//	}
-//
-//	public void setITotalDisplayRecords(Integer iTotalDisplayRecords) {
-//		this.iTotalDisplayRecords = iTotalDisplayRecords;
-//	}
-//
-//	public Integer getiTotalRecords() {
-//		return iTotalRecords;
-//	}
-//
-//	public void setITotalRecords(Integer iTotalRecords) {
-//		this.iTotalRecords = iTotalRecords;
-//	}
-//
-//	public Integer getsEcho() {
-//		return sEcho;
-//	}
-//
-//	public void setSEcho(Integer sEcho) {
-//		this.sEcho = sEcho;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Page [sEcho=" + sEcho + ", iTotalDisplayRecords="
-//				+ iTotalDisplayRecords + ", iDisplayLength=" + iDisplayLength
-//				+ ", iTotalRecords=" + iTotalRecords + "]";
-//	}
-//
-//	public List<T> getData() {
-//		return data;
-//	}
-//
-//	public void setData(List<T> data) {
-//		this.data = data;
-//	}
-//
 
 }
