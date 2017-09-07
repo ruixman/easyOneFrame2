896fabaff5921f68e0ded9d3f88012eec7d5967a
ruixman <ruixman@qq.com>
2017-09-07 13:34:43 +0800

    rename file

--- src/main/java/com/hfzs/biz/cms/repository/CmsArticleDao.java
+++ src/main/java/com/hfzs/biz/cms/repository/CmsArticleDao.java
@@ -3,9 +3,13 @@ package com.hfzs.biz.cms.repository;
 import com.hfzs.biz.cms.domain.CmsArticle;
 import com.hfzs.framework.core.web.orm.MyJpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/8/30.
  */
 public interface CmsArticleDao extends MyJpaRepository<CmsArticle,String> {
     public CmsArticle findById(String id);
+
+    public List<CmsArticle> findByTitle(String title);
 }
